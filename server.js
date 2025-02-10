1f2a8784e2bc642673f38fe0709c718ec2402d67
K B Venkataramana <venkataramanakbala@gmail.com>
2025-02-10 14:27:50 +0530

    will now work in local host

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const framesDir = path.join(__dirname, "frames", "parrot");
 
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') { // Check if the request is HTTP
-      return res.redirect(301, `https://${req.hostname}${req.originalUrl}`); // Redirect to HTTPS
-    }
-    next(); // Continue to the next middleware/route handler
-});
+// app.use((req, res, next) => {
+//     if (req.headers['x-forwarded-proto'] !== 'https') { // Check if the request is HTTP
+//       return res.redirect(301, `https://${req.hostname}${req.originalUrl}`); // Redirect to HTTPS
+//     }
+//     next(); // Continue to the next middleware/route handler
+// });
 
 function getFrames() {
     return fs.readdirSync(framesDir)
@@ -22,42 +22,47 @@ function getFrames() {
         .sort();
 }
 
-app.get("/", (req, res) => {
-    res.setHeader("Content-Type", "text/plain");
-    res.send("Welcome");
-});
-
 // app.get("/", (req, res) => {
-//     return res.send(`
-//         <html>
-//             <head>
-//                 <title>Console Canvas</title>
-//                 <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-//                 <style>
-//                     body {
-//                         background-color: black;
-//                         color: white;
-//                         font-family: Arial, sans-serif;
-//                         text-align: center;
-//                     }
-//                     code {
-//                         background-color: #333;
-//                         padding: 5px;
-//                         border-radius: 5px;
-//                         color: #0f0;
-//                     }
-//                 </style>
-//             </head>
-//             <body>
-//                 <h1>Welcome!</h1>
-//                 <p>This is project is for you to have fun while using your terminal</p>
-//                 <code>curl -L consolecanvas.vercel.app/parrot</code>
-//             </body>
-//         </html>
-//     `);
-    
-    
+//     res.setHeader("Content-Type", "text/plain");
+//     res.send("Welcome");
 // });
+
+app.get("/", (req, res) => {
+    const userAgent = req.headers["user-agent"] || "";
+    if (!userAgent.includes("curl")){
+        return res.send(`
+        <html>
+            <head>
+                <title>Console Canvas</title>
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+                <style>
+                    body {
+                        background-color: black;
+                        color: white;
+                        font-family: Arial, sans-serif;
+                        text-align: center;
+                    }
+                    code {
+                        background-color: #333;
+                        padding: 5px;
+                        border-radius: 5px;
+                        color: #0f0;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Welcome!</h1>
+                <p>This is project is for you to have fun while using your terminal</p>
+                <code>curl -L consolecanvas.vercel.app/parrot</code>
+            </body>
+        </html>
+    `);
+    }
+    else{
+        res.send('Use [/parrot]');
+    }
+    
+});
 
 app.get("/parrot", (req, res) => {
     const userAgent = req.headers["user-agent"] || "";
